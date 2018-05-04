370b3721bb6743cc6b8431485e5da2b70fb15633
Hwan Seob Lee <bestseob93@gmail.com>
2018-05-04 13:52:40 +0900

    next react added && pages added

---
+++ pages/NewCampaign.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NewCampaign() {
+  return (
+    <div>This is the NewCampaign Page!</div>
+  );
+}
+
+export default NewCampaign;
---
+++ pages/Show.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Show() {
+  return (
+    <div>Welcome! This is the Show Page!</div>
+  );
+}
+
+export default Show;
